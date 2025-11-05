ffba7a055861f5f751ee45f6acd0130029343f5d
Srinath <srinath97>
2025-11-06 00:56:01 +0530

    Add lovable-tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from "lovable-tagger";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
